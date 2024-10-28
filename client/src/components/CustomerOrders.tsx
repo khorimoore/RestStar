a17c05ffaa0e8e8e0a908ee7de632034ad0e44e3
khori moore <khorimoore23@gmail.com>
2024-10-28 08:46:58 -0700

    updated code structure on CustomerOrders.tsx file

--- client/src/components/CustomerOrders.tsx
+++ client/src/components/CustomerOrders.tsx
@@ -26,11 +26,13 @@ const CustomerList: React.FC<CustomerOrderProps> = () => {
       }
     };
 
+
     fetchOrders(); 
   }, []);
 
   if (loading) return <div>Loading...</div>; 
   if (error) return <div>{error}</div>; 
+  
 
   return (
     <div>
